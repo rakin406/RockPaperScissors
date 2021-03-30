d01c6e088999057cfa861da082e840d8f1614eaf
rakin406 <rakinhacker@gmail.com>
2021-03-30 20:46:53 +0600

    Add TODO

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,14 @@ const int NUM_OF_MOVES = 3;
 const std::string MOVES[NUM_OF_MOVES] = {"Rock", "Paper", "Scissors"};
 const std::string HELP = "r for rock, p for paper, s for scissors";
 
+// TODO: Process input, generate computer move.
 int main() {
-    std::string user_move;
     std::cout << "Instructions:\n";
     std::cout << HELP << "\n";
     std::cout << ROUNDS << " rounds!\n\n";
 
+    // Game loop
+    std::string user_move;
     for (int i = 0; i < ROUNDS; ++i) {
         std::cout << ">> ";
         std::cin >> user_move;
