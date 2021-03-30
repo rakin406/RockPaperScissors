a37325ede6e1a9d24ec4b5ea65ee67b4dfd90e97
rakin406 <rakinhacker@gmail.com>
2021-03-30 20:41:50 +0600

    Add instructions for commands

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,13 @@
 const int ROUNDS = 10;
 const int NUM_OF_MOVES = 3;
 const std::string MOVES[NUM_OF_MOVES] = {"Rock", "Paper", "Scissors"};
+const std::string HELP = "r for rock, p for paper, s for scissors";
 
 int main() {
     std::string user_move;
     std::cout << ROUNDS << " rounds!\n\n";
+    std::cout << "Instructions:\n";
+    std::cout << HELP << "\n\n";
 
     for (int i = 0; i < ROUNDS; ++i) {
         std::cout << ">> ";
