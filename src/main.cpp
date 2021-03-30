fa12e4f99785e1fab283e0aab38b8431f0320da7
rakin406 <rakinhacker@gmail.com>
2021-03-30 20:32:17 +0600

    Add all moves in constant array

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+const int NUM_OF_MOVES = 3;
+const std::string MOVES[NUM_OF_MOVES] = {"Rock", "Paper", "Scissors"};
+
+int main() {
+    return 0;
+}
