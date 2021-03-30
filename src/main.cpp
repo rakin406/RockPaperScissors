9ebfd9850364fc544ffbf8eb9652b420f428b1ac
rakin406 <rakinhacker@gmail.com>
2021-03-30 21:15:54 +0600

    Generate random moves

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@ const int NUM_OF_MOVES = 3;
 const std::string MOVES[NUM_OF_MOVES] = {"Rock", "Paper", "Scissors"};
 const std::string HELP = "r for rock, p for paper, s for scissors";
 
-// TODO: Process input, generate computer move.
 int main() {
     srand(time(0)); // Used for random number generation
     std::cout << "Instructions:\n";
@@ -15,16 +14,23 @@ int main() {
     std::cout << ROUNDS << " rounds!\n\n";
 
     // Game loop
-    std::string user_move;
+    std::string user_move, comp_move;
     int user_wins = 0, comp_wins = 0;
     for (int i = 0; i < ROUNDS; ++i) {
         std::cout << ">> ";
         std::cin >> user_move;
+
+        // Check user moves
+        comp_move = MOVES[rand() % NUM_OF_MOVES];
         if (user_move == "r" || user_move == "R") {
-            if (MOVES[rand() % NUM_OF_MOVES] == MOVES[0]) {
+            if (comp_move == MOVES[0]) {
                 std::cout << "Tie\n";
-            } else if (MOVES[rand() % NUM_OF_MOVES] == MOVES[1]) {
-            } else if (MOVES[rand() % NUM_OF_MOVES] == MOVES[2]) {
+            } else if (comp_move == MOVES[1]) {
+                std::cout << "Computer wins\n";
+                ++comp_wins;
+            } else if (comp_move == MOVES[2]) {
+                std::cout << "User wins\n";
+                ++user_wins;
             }
         }
     }
