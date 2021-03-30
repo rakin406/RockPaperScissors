677139f868b5241822cb125f8d6ec5000dcd2f69
rakin406 <rakinhacker@gmail.com>
2021-03-30 20:39:22 +0600

    Add rounds

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,18 @@
 #include <iostream>
 #include <string>
 
+const int ROUNDS = 10;
 const int NUM_OF_MOVES = 3;
 const std::string MOVES[NUM_OF_MOVES] = {"Rock", "Paper", "Scissors"};
 
 int main() {
+    std::string user_move;
+    std::cout << ROUNDS << " rounds!\n\n";
+
+    for (int i = 0; i < ROUNDS; ++i) {
+        std::cout << ">> ";
+        std::cin >> user_move;
+    }
+
     return 0;
 }
